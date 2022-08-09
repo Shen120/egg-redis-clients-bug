e3024f19854bc761891b537917a563c519950eb9
Shen120 <33613409+Shen120@users.noreply.github.com>
2022-08-09 09:12:10 +0800

    Update home.ts

--- app/controller/home.ts
+++ app/controller/home.ts
@@ -1,9 +1,10 @@
 import { Controller } from 'egg';
+import "egg-redis/index";
 
 export default class HomeController extends Controller {
   public async index() {
     const { ctx, app } = this;
-    app.redis.get("def").set("test", 11111)
+    await app.redis.get("def").set("test", 11111)
     ctx.body = await ctx.service.test.sayHi('egg');
   }
 }
